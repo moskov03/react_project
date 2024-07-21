6adbcecc03e50a7de8c428c3449d394ffd9e9ea9
moskov03 <dimitarmoskov3@gmail.com>
2024-07-21 11:48:26 +0300

    added home content

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import Header from "./components/header/Header"
 import Footer from "./components/footer/Footer"
+import Home from "./components/home/Home"
 function App() {
 
   return (
     <>
       <Header />
       <main>
-        <br />
+        <Home />
       </main>
       <Footer />
     </>
---
+++ client/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+export default function Component() {
+    return (
+        <>
+            <div className="content">
+                <h1>Welcome to the Dog Adoption Center</h1>
+                <p>Find your new best friend today! Our dog adoption center helps connect loving families with wonderful dogs in need of a home.</p>
+                <a href="#" className="catalog">View Available Dogs</a>
+            </div>
+        </>
+    );
+}
