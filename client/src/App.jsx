298260734c7ca0d24466c931c4596272c58075bb
moskov03 <dimitarmoskov3@gmail.com>
2024-07-21 12:50:19 +0300

    Routes
    Added Routes and Login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,21 @@
+import { Routes, Route } from 'react-router-dom'
+
 import Header from "./components/header/Header"
 import Footer from "./components/footer/Footer"
 import Home from "./components/home/Home"
-function App() {
+import Login from './components/login/Login'
 
+function App() {
   return (
     <>
       <Header />
       <main>
-        <Home />
+      
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/login' element={<Login />}/>
+        </Routes>
+
       </main>
       <Footer />
     </>
