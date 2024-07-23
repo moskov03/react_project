86f9b1b229736bcf75fc36c175077c82de6fa012
moskov03 <dimitarmoskov3@gmail.com>
2024-07-23 11:24:36 +0300

    Update Home.jsx
    minor fixes

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Component() {
     return (
         <>
             <div className="content">
                 <h1>Welcome to the Dog Adoption Center</h1>
                 <p>Find your new best friend today! Our dog adoption center helps connect loving families with wonderful dogs in need of a home.</p>
-                <a href="/catalog" className="catalog">Find your puppy</a>
+                <Link to="/catalog" className="catalog">Find your puppy</Link>
             </div>
         </>
     );
