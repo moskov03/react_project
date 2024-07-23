9e0a417d33dca827d2855851c63fa57e98f7d43b
moskov03 <dimitarmoskov3@gmail.com>
2024-07-23 15:31:22 +0300

    Connected frontend to backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import axios from 'axios'
 
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
@@ -9,11 +10,18 @@ import Register from './components/register/Register'
 import Catalog from './components/catalog/Catalog'
 
 function App() {
+
+  const apiCall = () => {
+    axios.get('http://localhost:5050').then(() => {
+      console.log('works apiCall');
+    })
+  }
+
   return (
     <>
       <Header />
       <main>
-      
+      <button onClick={apiCall}>TEST</button>
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/login' element={<Login />}/>
