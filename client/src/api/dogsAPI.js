ee7b27c3dea1cdcaf60a40195752ad253387a451
moskov03 <dimitarmoskov3@gmail.com>
2024-07-27 12:18:41 +0300

    Get all request added
    for loading all dogs stored in the collection if any

--- client/src/api/dogsAPI.js
+++ client/src/api/dogsAPI.js
@@ -1,2 +1,8 @@
 //crud zaqvki tuk
+import * as request from './requester'
 
+const BASE_URL = 'http://localhost:3030/jsonstore/dogs'
+
+export const getAll = async () =>{
+    await request.get(BASE_URL)
+}
