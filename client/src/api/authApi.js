82232126a7a225e1138a38473a39066364b86bbc
moskov03 <dimitarmoskov3@gmail.com>
2024-08-07 13:36:47 +0300

    localstorage

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -3,6 +3,4 @@ import requester from "./requester"
 const BASE_URL = 'http://localhost:3030/users'
 
 
-export const login = (email, password) => requester.post(`${BASE_URL}/login`, { email, password });
-
 export const register = (email, password) => requester.post(`${BASE_URL}/register`, { email, password });
