51aa6f209f69ad9d9521c71c63e529f77c5e4f01
moskov03 <dimitarmoskov3@gmail.com>
2024-08-09 11:30:33 +0300

    Update usePersistedState.js

--- client/hooks/usePersistedState.js
+++ client/hooks/usePersistedState.js
@@ -21,8 +21,12 @@ export default function usePersistedState(key, initialState) {
             ? value(state)
             : value;
 
+        if (newState === null || newState === undefined) {
+            localStorage.removeItem(key)
+        } else {
+            localStorage.setItem(key, JSON.stringify(newState))
 
-        localStorage.setItem(key, JSON.stringify(newState))
+        }
 
         setState(newState)
     }
