dd06736a8983390ab18685bda1bdb4556a9e237c
moskov03 <dimitarmoskov3@gmail.com>
2024-08-18 09:57:13 +0300

    Update Catalog.jsx

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -1,41 +1,21 @@
-// import Card from './Card';
-// import { useGetAllDogs } from '../../../hooks/useDogs';
-// // setDogs
-// export default function Catalog() {
-//     const [dogs, ] = useGetAllDogs()
-//     return (
-//         <>
-//             <div className="catalog">
-
-//                 {dogs.length > 0
-//                     ? dogs.map(dog => <Card {...dog} key={dog._id} />)
-//                     : <h3 className='not-available'>All puppies have home yay!</h3>
-//                 }
-
-//             </div>
-//         </>
-//     );
-// }
-
 
 import  { useState } from 'react';
 import Card from './Card';
 import { useGetAllDogs } from '../../../hooks/useDogs';
-import Pagination from './Pagination';  // You will create this component
-
+import Pagination from './Pagination';  
 export default function Catalog() {
     const [dogs] = useGetAllDogs();
     const [currentPage, setCurrentPage] = useState(1);
     const dogsPerPage = 6;
 
-    // Calculate the indexes of the first and last dogs on the current page
+    
     const indexOfLastDog = currentPage * dogsPerPage;
     const indexOfFirstDog = indexOfLastDog - dogsPerPage;
 
-    // Slice the dogs array to get only the dogs for the current page
+    
     const currentDogs = dogs.slice(indexOfFirstDog, indexOfLastDog);
 
-    // Function to change the page
+   
     const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
     return (
